d472d2ae3d5ea1e59611edb3870daf1f4c394a49
Robin Lee <rgl@google.com>
2014-11-27 20:33:24 +0000

    Shorten findmin

--- code/snippets/various/findmin.java
+++ code/snippets/various/findmin.java
@@ -2,41 +2,6 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-  static int[][] queries;
-  static int arrayLength;
-  static int numQueries;
-  static int[] arr;
-  static Node tree = null;
-
-  public static void main(String[] args) throws IOException{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    String[] lineOne = br.readLine().split(" ");
-    arrayLength = Integer.parseInt(lineOne[0]);
-    numQueries = Integer.parseInt(lineOne[1]);
-
-    String[] lineTwo = br.readLine().split(" ");
-
-    arr = new int[arrayLength];
-    for (int i = 0; i < arrayLength; i++){
-      arr[i] = Integer.parseInt(lineTwo[i]);
-    }
-
-    //build a tree from the array
-    tree = buildTree(arr);
-
-    // process these in realtime
-    for (int i = 0; i < numQueries; i++){
-
-      String[] query = br.readLine().split(" ");
-
-      int low = Integer.parseInt(query[0]);
-      int high = Integer.parseInt(query[1]);
-
-      System.out.println(rangeMinimumQuery(tree, low, high));
-    }
-  }
-
   public static Node buildTree(int[] arr){
     LinkedList<Node> queueOne = new LinkedList<Node>();
     LinkedList<Node> queueTwo = new LinkedList<Node>();
@@ -49,17 +14,10 @@ public class Main {
 
     while (queueOne.size() > 1){
       while (queueOne.size() > 0){
-        if (queueOne.size() == 1){
-          queueTwo.add(queueOne.poll());
-        }
-        else{
-          Node tempOne = queueOne.poll();
-          Node tempTwo = queueOne.poll();
-
+        if (queueOne.size() == 1) queueTwo.add(queueOne.poll()); else{
+          Node tempOne = queueOne.poll(), tempTwo = queueOne.poll();
           int min = tempOne.min < tempTwo.min ? tempOne.min : tempTwo.min;
-
           Node parent = new Node(min, tempOne.lowIx, tempTwo.highIx, tempOne, tempTwo);
-
           queueTwo.add(parent);
         }
       }
@@ -78,23 +36,15 @@ public class Main {
       return Integer.MAX_VALUE;
     }
     else if ((parent.lowIx >= startIx && parent.highIx <= endIx)){
-      //if the range of the current node is fully contained within it
-
-      //return min of range
-
+      //if the range of the current node is fully contained within it return min of range
       return parent.min;
     }
     else if (rangeOverlap(parent, startIx, endIx)){
-      //if the range overlaps the total range
-
-      Node left = parent.leftChild;
-      Node right = parent.rightChild;
-
+      // if the range overlaps the total range
+      Node left = parent.leftChild, right = parent.rightChild;
       return (Math.min(rangeMinimumQuery(left, startIx, endIx), rangeMinimumQuery(right, startIx, endIx)));
     }
-    else{
-      return Integer.MAX_VALUE;
-    }
+    return Integer.MAX_VALUE;
   }
 
   public static boolean rangeOverlap(Node node, int lowIx, int highIx){
@@ -102,28 +52,15 @@ public class Main {
   }
 
   private static class Node{
-    int min;
-    int lowIx;
-    int highIx;
-    Node leftChild = null;
-    Node rightChild = null;
-
+    int min, lowIx, highIx;
+    Node leftChild = null, rightChild = null;
     public Node(int min, int lowIx, int highIx, Node leftChild, Node rightChild){
-      this.min = min;
-      this.lowIx = lowIx;
-      this.highIx = highIx;
-      this.leftChild = leftChild;
-      this.rightChild = rightChild;
+      this.min = min; this.lowIx = lowIx; this.highIx = highIx;
+      this.leftChild = leftChild; this.rightChild = rightChild;
     }
-
     public Node(int min, int lowIx, int highIx){
-      this.min = min;
-      this.lowIx = lowIx;
-      this.highIx = highIx;
+      this.min = min; this.lowIx = lowIx; this.highIx = highIx;
     }
-
-    public String toString(){
-      return Integer.toString(min);
-    }
+    public String toString() {return Integer.toString(min);}
   }
 }
